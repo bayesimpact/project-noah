7f4af712ea8a1a9a0b5183246073248c1321e352
Stephan Gabler <stephan.gabler@gmail.com>
2017-02-22 13:21:51 +0100

    Add a map of London to the landing page.

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -2,6 +2,9 @@ require('normalize.css/normalize.css')
 require('styles/App.css')
 
 import React from 'react'
+import ReactMapboxGl, {Layer, Feature} from 'react-mapbox-gl'
+
+import config from 'config'
 
 
 class AppComponent extends React.Component {
@@ -12,13 +15,30 @@ class AppComponent extends React.Component {
       justifyContent: 'center',
       flexDirection: 'column',
     }
+    const mapboxContainerStyle = {
+      height: '100vh',
+      width: '100vw',
+    }
     return (
       <div style={appStyle}>
         <h1>Agile Prototype!</h1>
         <h2>This is going to be amazing!</h2>
+        <ReactMapboxGl
+          style="mapbox://styles/mapbox/streets-v8"
+          accessToken={config.mapboxAccessToken}
+          center={[-0.481747846041145, 51.3233379650232]}
+          containerStyle={mapboxContainerStyle}>
+          <Layer
+              type="symbol"
+              id="marker"
+              layout={{'icon-image': 'marker-15'}}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          </Layer>
+        </ReactMapboxGl>
       </div>
     )
   }
 }
+
 
 export default AppComponent
--- frontend/src/config/base.js
+++ frontend/src/config/base.js
@@ -2,4 +2,6 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
+  mapboxAccessToken: 'pk.eyJ1IjoiZGVkYW4iLCJhIjoiYWRiYjQ2Y2Y0ODQ0ZGI1ZmEwOWU4NTY4NDE3NzRlZGYifQ.' +
+    'BSRTH52c6yZgkNaDipxqyg',
 }
