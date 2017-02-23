73bf2e06d6b757e57a4d53a5531f9466466a53eb
Stephan Gabler <stephan.gabler@gmail.com>
2017-02-23 19:27:27 +0100

    Highlight users in the proximity of a hazard when hovering the hazard in admin view.

--- frontend/src/components/admin_view.jsx
+++ frontend/src/components/admin_view.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactMapboxGl, {Layer, Marker} from 'react-mapbox-gl'
+import haversine from 'haversine'
 
 import config from 'config'
 import {store} from 'store/firebase'
 
 
-// Center of the US.
+// Center of the US (lng, lat).
 const START_LOCATION = [-96.328530, 38.321018]
+const PROXIMITY_THRESHOLD = 50
 
 
 class AdminView extends React.Component {
@@ -15,34 +17,82 @@ class AdminView extends React.Component {
   };
 
   componentWillMount() {
-    store.getUserProfiles(userProfiles => this.setState({userProfiles}))
-    store.getHazards(hazards => this.setState({hazards}))
+    store.getUserProfiles(userProfiles => {
+      this.setState({userProfiles}, this.computeProximity)
+    })
+    store.getHazards(hazards => {
+      this.setState({hazards}, this.computeProximity)
+    })
   }
 
-  state = {}
+  distanceBetweenCoordinatesKm(coord1, coord2) {
+    return haversine(
+      {longitude: coord1[0], latitude: coord1[1]},
+      {longitude: coord2[0], latitude: coord2[1]},
+      {unit: 'meter'}) / 1000
+  }
+
+  computeProximity() {
+    const {hazards, userProfiles} = this.state
+    const hazardProximity = {}
+    if (!hazards || !userProfiles) {
+      return
+    }
+    for (var i = 0; i < (hazards || []).length; i++) {
+      for (var j = 0; j < userProfiles.length; j++) {
+        const distKm = this.distanceBetweenCoordinatesKm(hazards[i], userProfiles[j].location)
+        if (distKm < PROXIMITY_THRESHOLD) {
+          hazardProximity[i] = (hazardProximity[i] || []).concat([j])
+        }
+      }
+    }
+    this.hazardProximity = hazardProximity
+  }
+
+  state = {
+    zoomLevel: 4,
+  }
 
   render() {
-    const {hazards, userProfiles} = this.state
+    const {hazards, hoveredHazard, userProfiles, zoomLevel} = this.state
     const mapboxContainerStyle = {
       height: '100vh',
       width: '100vw',
     }
+    const markerStyle = {
+      width: 10,
+      height: 10,
+      borderRadius: '50%',
+      backgroundColor: '#E0E0E0',
+      border: '2px solid #C9C9C9',
+    }
     return (
       <div>
         <h1>Welcome Admin</h1>
+        <h2>
+          Hover over the hazards to highlight people in its proximity ({PROXIMITY_THRESHOLD} Km)
+        </h2>
         <ReactMapboxGl
             style="mapbox://styles/mapbox/streets-v8"
             accessToken={config.mapboxAccessToken}
             center={START_LOCATION}
             containerStyle={mapboxContainerStyle}
-            zoom={[4]}>
-          <Layer type="symbol" id="users" layout={{'icon-image': 'marker-15'}}>
-            {(userProfiles || []).map((userProfile, i) => {
-              return <Marker key={i} coordinates={userProfile.location} />
-            })}
-          </Layer>
+            onZoom={map => this.setState({zoomLevel: map.getZoom()})}
+            zoom={[zoomLevel]}>
+          {(userProfiles || []).map((userProfile, i) => {
+            const isInProximity = this.hazardProximity &&
+              hoveredHazard && (this.hazardProximity[hoveredHazard] || []).includes(i)
+            return <Marker
+                key={i} coordinates={userProfile.location}
+                style={{...markerStyle, backgroundColor: isInProximity ? 'red' : '#E0E0E0'}} />
+          })}
           <Layer type="symbol" id="hazards" layout={{'icon-image': 'fire-station-15'}}>
-            {(hazards || []).map((coordinates, i) => <Marker key={i} coordinates={coordinates} />)}
+            {(hazards || []).map((coordinates, i) => {
+              return <Marker
+                  key={i} coordinates={coordinates}
+                  onHover={() => this.setState({hoveredHazard: i})}
+                  onEndHover={() => this.setState({hoveredHazard: null})} />
+            })}
           </Layer>
         </ReactMapboxGl>
       </div>
