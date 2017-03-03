26a20292826b4b96702d232aa7077a552dcc918f
Stephan Gabler <stephan.gabler@gmail.com>
2017-03-03 12:02:47 +0100

    Add continuous deployment of our notification handler to the ECS.

---
+++ backend/restart-aws-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A script that restarts the Notification Handler backend service on AWS. It does it by
+# killing the only task in this service: the service will immediately create a
+# new one.
+#
+# Do this if the Docker image has been updated and you want to use the latest
+# one. The downside is that there is some down time, but given the current
+# design of the backend this is completly acceptable.
+readonly AWS_SERVICE=project-noah
+readonly RESTART_REASON="$1"
+
+readonly TASK_ID="$(aws ecs list-tasks --service-name ${AWS_SERVICE} |\
+  grep arn |\
+  sed -e "s/^.*task\///;s/\".*//")"
+aws ecs stop-task --task "${TASK_ID}" --reason "${RESTART_REASON}"
